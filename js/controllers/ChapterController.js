8b86c3519f2161d41023d306f8dee23e625fc967
unknown <intern@Emooth112-PC.emooth.com>
2015-05-20 16:07:51 +1000

    + Pagination

--- js/controllers/ChapterController.js
+++ js/controllers/ChapterController.js
@@ -1,6 +1,8 @@
 app.controller('ChapterController', ['$scope', 'books', '$routeParams', function($scope, books, $routeParams) {
 	books.success(function(data) {
 		$scope.book = data[$routeParams.bookId];
+		$scope.currentBookIndex = $routeParams.bookId;
+		$scope.nextChapterIndex = (parseInt($routeParams.chapterId) + 1) % data[$routeParams.bookId].chapters.length;
 		$scope.chapters = data[$routeParams.bookId].chapters[$routeParams.chapterId];
 	});
 }]);
